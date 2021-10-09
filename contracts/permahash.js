42e68f10d4c99e4c532532ee6e6867848f9ef709
Derek 💫 <arein@paypal.com>
2021-10-09 12:20:39 -0400

    Uploads hashes to Smartweave

--- contracts/permahash.js
+++ contracts/permahash.js
@@ -1,12 +1,15 @@
 export function handle(state, action) {
     if (action.input.function === 'index') {
+        
         if (typeof action.input.hash !== 'string' || action.input.hash.length < 3) {
           throw new ContractError(`Invalid hash provided: ${action.input.hash}`)
         }
-        state.hashes.append = {
+        
+        state.hashes.push({
           author: action.caller,
-          hash: action.input.hash
-        }
+          hash: action.input.hash,
+          createdAt: SmartWeave.block.timestamp
+        });
     
         return { state }
     }
--- src/App.js
+++ src/App.js
@@ -1,35 +1,84 @@
 import './App.css';
 import React from 'react';
 import ImageUploading from 'react-images-uploading';
+import { readContract, interactWrite } from 'smartweave';
+import Arweave from 'arweave';
 
 
 function App() {
 
   const [images, setImages] = React.useState([]);
-const [imageHash, setImageHash] = React.useState([]);
-const maxNumber = 1;
+  const [imageHash, setImageHash] = React.useState([]);
+  const [existingHash, setExistingHash] = React.useState([]);
+  const maxNumber = 1;
+  const contractId = "TTRQsfcDB50dYOSJZ3CL5KDTfVs2rEIQ6Lxm6rDAUEc";
+  const arweave = Arweave.init({});
 
-const onChange = (imageList, addUpdateIndex) => {
-  // data for submit
-  setImages(imageList);
-};
+  const onChange = (imageList, addUpdateIndex) => {
+    // data for submit
+    setImages(imageList);
+  };
+
+  readContract(arweave, contractId).then((state) => {
+    console.log(state);
+  });
+
+  const lookupImageHash = (hash) => {
+    readContract(arweave, contractId).then((state) => {
+      console.log(state);
+      const found = state.hashes.filter((existingHash) => {
+        return existingHash.hash === hash;
+      });
+      if (found.length > 0) {
+        console.log(found[0]);
+        setExistingHash(found[0]);
+      }
+    });
+  };
 
   if (images.length > 0) {
     const base64 = images[0].data_url.replace(/^data:image\/(png|jpg);base64,/, "");
     crypto.subtle.digest("SHA-256", new TextEncoder("utf-8").encode(base64)).then((buf) => {
-      setImageHash(Array.prototype.map.call(new Uint8Array(buf), x=>(('00'+x.toString(16)).slice(-2))).join(''));
+      const newHash = Array.prototype.map.call(new Uint8Array(buf), x=>(('00'+x.toString(16)).slice(-2))).join('');
+      if (imageHash !== newHash) {
+        setExistingHash(false);
+        setImageHash(newHash);
+        lookupImageHash(newHash);
+      }
     });
   }
+
+  const createPermahash = () => {
+    console.log("Storing hash", imageHash);
+    const input = {
+      'function': 'index',
+      'hash': imageHash
+    };
+    interactWrite(arweave, null, contractId, input).then((result) => {
+      console.log('arweave status', result);
+      readContract(arweave, contractId).then((state) => {
+        console.log(state);
+      });
+    });
+  };
 
   return (
     <div className="App">
             {images.length > 0 &&
-       <p>
-          THe permahash of your image is '{imageHash}' 
+       <div>
+          The permahash of your image is '{imageHash}' 
           <span role="img" aria-label="Nerd smiley emoji">
             🤓
           </span>
-        </p>
+          <br />
+          {existingHash && <p>
+              It already exists and was created by {existingHash.author} at {existingHash.createdAt} 
+            <span role="img" aria-label="Nerd smiley emoji">
+              🤓
+            </span>
+          </p>
+          }
+        </div>
       }
       <ImageUploading
         multiple
@@ -49,15 +98,16 @@ const onChange = (imageList, addUpdateIndex) => {
         }) => (
           // write your building UI
           <div className="upload__image-wrapper">
-            <button
+            {images.length === 0 && <button
               style={isDragging ? { color: 'red' } : undefined}
               onClick={onImageUpload}
               {...dragProps}
             >
               Click or Drop here
             </button>
+            }
             &nbsp;
-            <button onClick={onImageRemoveAll}>Remove</button>
+            {images.length > 0 && <button onClick={onImageRemoveAll}>Remove</button>}
             {imageList.map((image, index) => (
               <div key={index} className="image-item">
                 <img src={image['data_url']} alt="" width="100" />
@@ -66,6 +116,10 @@ const onChange = (imageList, addUpdateIndex) => {
           </div>
         )}
       </ImageUploading>
+      {images.length > 0 &&
+       <button onClick={createPermahash}>Store Hash Permanently</button>
+      }
+
     </div>
   );
 }
