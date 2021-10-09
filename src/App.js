b639d6fe27cd6e2f618d299fe900aae19ebff7ed
Derek 💫 <arein@paypal.com>
2021-10-09 09:43:18 -0400

    Adds Hashing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ImageUploading from 'react-images-uploading';
+
 
 function App() {
+
+  const [images, setImages] = React.useState([]);
+const [imageHash, setImageHash] = React.useState([]);
+const maxNumber = 1;
+
+const onChange = (imageList, addUpdateIndex) => {
+  // data for submit
+  setImages(imageList);
+};
+
+  if (images.length > 0) {
+    const base64 = images[0].data_url.replace(/^data:image\/(png|jpg);base64,/, "");
+    crypto.subtle.digest("SHA-256", new TextEncoder("utf-8").encode(base64)).then((buf) => {
+      setImageHash(Array.prototype.map.call(new Uint8Array(buf), x=>(('00'+x.toString(16)).slice(-2))).join(''));
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+            {images.length > 0 &&
+       <p>
+          THe permahash of your image is '{imageHash}' 
+          <span role="img" aria-label="Nerd smiley emoji">
+            🤓
+          </span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      }
+      <ImageUploading
+        multiple
+        value={images}
+        onChange={onChange}
+        maxNumber={maxNumber}
+        dataURLKey="data_url"
+      >
+        {({
+          imageList,
+          onImageUpload,
+          onImageRemoveAll,
+          onImageUpdate,
+          onImageRemove,
+          isDragging,
+          dragProps,
+        }) => (
+          // write your building UI
+          <div className="upload__image-wrapper">
+            <button
+              style={isDragging ? { color: 'red' } : undefined}
+              onClick={onImageUpload}
+              {...dragProps}
+            >
+              Click or Drop here
+            </button>
+            &nbsp;
+            <button onClick={onImageRemoveAll}>Remove</button>
+            {imageList.map((image, index) => (
+              <div key={index} className="image-item">
+                <img src={image['data_url']} alt="" width="100" />
+              </div>
+            ))}
+          </div>
+        )}
+      </ImageUploading>
     </div>
   );
 }
